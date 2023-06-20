66e8c0f0ec8bffe42c11b7afaf4e92d264eb7349
Prince Nuel <emmanuel.chukwuemeka0416@gmail.com>
2023-06-20 21:43:21 +0100

    Update validations.py
    I made corrections in line 5,(recieved to received), line 9 added, "be" and line 12 (ereturn to return).

--- validations.py
+++ validations.py
@@ -2,14 +2,14 @@
 
 
 def validate_user(username, minlen):
-    """Checks if the recieved username matches the required conditions."""
+    """ Checks if the received username matches the required conditions."""
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
-        raise ValueError("minlen must at least 1")
+        raise ValueError("minlen must at least be 1")
         
     if len(username) < minlen:
-        ereturn False
+        return False
     if not username.isalnum():
         return False
     # Usernames can't begin with a number
